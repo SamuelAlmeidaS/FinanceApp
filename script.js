687c1883a4d8d630e3d2bf4d31a3187d35c7c666
Samuel Almeida <75258537+SamuelAlmeidaS@users.noreply.github.com>
2022-09-08 17:05:23 -0300

    .

--- script.js
+++ script.js
@@ -2,22 +2,31 @@ window.addEventListener('load',(e)=>{
     e.preventDefault();
 
     const forms = document.querySelector('.forms');
+    forms.addEventListener("click", (e)=>{
+        e.preventDefault();
+        
+    });
+    
     const transition = document.querySelector('.new-transition');
     transition.addEventListener("click", ()=>{
-
-        if(forms.style.display == 'none'){
-            forms.style.display = 'flex';
-        } else {
+    
+        if(forms.style.display == 'flex'){
             forms.style.display = 'none';
+        } else {
+            forms.style.display = 'flex';
         }
         
     });
 
+    const money_input = document.querySelector('.money-input');
     const input_value_name = document.querySelector('.name-value-input');
     const input_value = document.querySelector('.value-input');
     const bnt_input = document.querySelector('.bnt-input');
     const bnt_output = document.querySelector('.bnt-output');
-
-    console.log(bnt_output)
+    
+    
+    bnt_input.addEventListener('click', ()=>{
+      
+    });
     
 });
