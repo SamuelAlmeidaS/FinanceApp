4271eae9c53a4f777cf99e0fffee7100104b4641
Samuel Almeida <75258537+SamuelAlmeidaS@users.noreply.github.com>
2022-09-09 14:02:25 -0300

    .

--- script.js
+++ script.js
@@ -19,14 +19,28 @@ window.addEventListener('load',(e)=>{
     });
 
     const money_input = document.querySelector('.money-input');
+    const money_output = document.querySelector('.money-output');
+    const money_total = document.querySelector('.money-total');
     const input_value_name = document.querySelector('.name-value-input');
     const input_value = document.querySelector('.value-input');
     const bnt_input = document.querySelector('.bnt-input');
     const bnt_output = document.querySelector('.bnt-output');
+    let sum_input = 0;
+    let sub_output = 0;
+    let total = 0;
     
-    
-    bnt_input.addEventListener('click', ()=>{
-      
+    bnt_input.addEventListener('click', () =>{
+        sum_input += input_value.value;
+        money_input.innerText = `R$ ${parseFloat(sum_input)}`;
+        total += sum_input;
+        money_total.innerHTML = `R$ ${parseFloat(total)}`;
+    });
+
+    bnt_output.addEventListener('click', () =>{
+        sub_output -= input_value.value;
+        money_output.innerHTML = `R$ ${parseFloat(sub_output)}`;
+        total -= sub_output;
+        money_total.innerHTML = `R$ ${parseFloat(total)}`;
     });
     
 });
